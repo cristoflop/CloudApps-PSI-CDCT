5fc827d34a720682e6be22911950aaf12720157f
cristoflop <cristofer.lc.99@gmail.com>
2021-01-30 01:23:32 +0100

    Added test in planner

---
+++ planner/src/test/java/es/codeurjc/mastercloudapps/topo/DemoApplicationTests.java
@@ -0,0 +1,28 @@
+package es.codeurjc.mastercloudapps.topo;
+
+import es.codeurjc.mastercloudapps.planner.models.LandscapeResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties.StubsMode;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@AutoConfigureStubRunner(
+        ids = {"es.codeurjc.test:cdct_ejem:+:stubs:8080"},
+        stubsMode = StubsMode.LOCAL)
+class DemoApplicationTests {
+
+    @Test
+    void verify_book_service() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        LandscapeResponse response = restTemplate.getForObject("http://localhost:8080/", LandscapeResponse.class);
+
+        assertEquals("Recas", response.getId());
+        assertEquals("Flat", response.getLandscape());
+    }
+
+}
